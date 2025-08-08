ced0996064e377d0003ffdc42a2ca9112e3ef231
VIQ <pannetier.julien@gmail.com>
2025-08-08 10:48:11 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
-
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "class", // ← Ajout pour activer le mode sombre via une classe
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
